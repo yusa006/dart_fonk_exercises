d51f8b23d56efa21151c16a0b8f43b19357d9788
yusa006 <122738007+yusa006@users.noreply.github.com>
2023-01-25 20:43:11 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
+/*
 void main(){
   
   evliCiftler("Nilüfer", "Mustafa");
@@ -20,4 +20,19 @@ evliCiftler(String k1, String e1){
     cinsiyet = "Kadın"; 
   }
     print("Merhaba $k1 ve $e1, Çocugunuzun cinsiyeti $cinsiyet dir, Hayırlı Olsun");
+}*/
+
+
+
+void main(){
+  yaz("Muhammed Yuşa", "AKCAN", 27, "Tanıştığıma Memnum Oldum.", "Yaşındayım", "Benim Adım");
+  }
+yaz(String s1, String s2, int s3, String s4, String s5, String s6){
+    s1 = "Muhammed Yuşa";
+    s2 = "AKCAN";
+    s3 = 27;
+    s4 = "Tanıştığıma Memnum Oldum.";
+    s5 = "Yaşındayım";
+    s6 = "Benim Adım";
+      print("$s6 $s1 $s2, $s3 $s5, $s4");
 }
