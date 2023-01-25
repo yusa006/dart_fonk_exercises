133fe9a982e889bf02951e7dc242be91aa584b94
yusa006 <122738007+yusa006@users.noreply.github.com>
2023-01-25 20:39:07 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main(){
+  
+  evliCiftler("Nilüfer", "Mustafa");
+}
+
+evliCiftler(String k1, String e1){
+ 
+ int sans1 = 1;
+ int sans2 = 2;
+ String cinsiyet = "";
+ int sonuc = sans1 + Random().nextInt(sans2);
+  
+  if  (sonuc == 1){
+    cinsiyet = "Erkek";
+  }
+  else { 
+    cinsiyet = "Kadın"; 
+  }
+    print("Merhaba $k1 ve $e1, Çocugunuzun cinsiyeti $cinsiyet dir, Hayırlı Olsun");
+}
