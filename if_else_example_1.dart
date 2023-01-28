ca196be3751f2068f5d452801c58fd18f4ed30a1
yusa006 <122738007+yusa006@users.noreply.github.com>
2023-01-28 17:55:01 +0300

    if_else_example_1

---
+++ if_else_example_1.dart
@@ -0,0 +1,18 @@
+void main(){
+  sirala(331,623);
+}
+
+sirala(int deger1, int deger2){
+  
+  if (deger1<deger2){
+    print("DEGER 2 DAHA BÜYÜKTÜR !!!");
+  }
+  
+    else if (deger1==deger2){
+    print("DEĞERLER BİRBİRİNE EŞİTTİR !!!");
+  }
+  
+    else {
+    print("DEĞER 1 DAHA BÜYÜKTÜR !!!");
+  }
+ }
